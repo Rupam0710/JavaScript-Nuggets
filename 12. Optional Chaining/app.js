ef0507c797e2b454b16eca2a5db8962e844ccbcb
Rupam <rupampal48@gmail.com>
2023-07-14 20:57:47 +0530

    19th commit

--- 12. Optional Chaining/app.js
+++ 12. Optional Chaining/app.js
@@ -1,35 +1,47 @@
 //Optional Chaining
 
 const people = [
-    {
-        name : "rupam",
-        location : {street : "137/2 , Purba Sinthee Road " , timezone:{offset :"+05:30"}}  
+  {
+    name: "rupam",
+    location: {
+      street: "137/2 , Purba Sinthee Road ",
+      timezone: { offset: "+05:30" },
     },
-    {
-        name : "bob",
-        location : {street : "Christ Church , Wellington" } 
-    },
-    {
-        name : "Veda",
-        location : {street : "Marine Drive" , timezone:{offset :"+06:30"}}  
-    },
-    ]
-    
+  },
+  {
+    name: "bob",
+    location: { street: "Christ Church , Wellington" },
+  },
+  {
+    name: "Veda",
+    location: { street: "Marine Drive", timezone: { offset: "+06:30" } },
+  },
+];
+
 // people.forEach((person)=>{
 //     console.log(person.name);
-// })    
+// })
 
 //To display the offset timing of each object
-people.forEach((person)=>{
-    console.log(person.location   && person.location.timezone && person.location.timezone.offset);
-}) 
+people.forEach((person) => {
+  console.log(
+    person.location &&
+      person.location.timezone &&
+      person.location.timezone.offset
+  );
+});
 
 //To display the offset timing of each object and mark offset that is undefined with any default value
-people.forEach((person)=>{
-    console.log(person.location   && person.location.timezone && person.location.timezone.offset || "The offset is not defined");
-})   
+people.forEach((person) => {
+  console.log(
+    (person.location &&
+      person.location.timezone &&
+      person.location.timezone.offset) ||
+      "The offset is not defined"
+  );
+});
 
 //To display the offset timing of each object using optional chaining
-people.forEach((person)=>{
-    console.log(person?.location?.timezone?.offset);
-})    
+people.forEach((person) => {
+  console.log(person?.location?.timezone?.offset);
+});
---
+++ 19.Fetch Errors/app.js
@@ -0,0 +1,23 @@
+//  Javascript Nuggets
+// Fetch Errors
+// Only throws an error if cannot resolve.
+// Error Response still a response (400-500)
+
+const url = 'https://www.course-api.com/react-tours-project'
+
+const getTours = async()=>{
+    try {
+    const resp = await fetch(url);
+    if(!resp.ok){
+        const msg = `There is some error "${resp.status} ${resp.statusText}"`
+        throw new Error(msg); 
+    }
+    const tours = await resp.json();
+    console.log(tours);
+    }catch (error) {
+        console.log(error);
+    }
+}
+
+const btn = document.querySelector('btn');
+btn.addEventListener('click',getTours);
